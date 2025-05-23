bc08f1d92f448240cea6b08015d7868ef0a01796
Sudip-gt <sudippaudel6561@gmail.com>
2025-05-23 11:29:19 +0545

    deploy managment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.', // Set root to current directory
-  base: './', // Ensures relative paths (good for deployment in subfolders like GitHub Pages)
+  base: './day10_todoApp', // Ensures relative paths (good for deployment in subfolders like GitHub Pages)
   build: {
     outDir: 'dist', // Output directory
     emptyOutDir: true, // Clean before build
